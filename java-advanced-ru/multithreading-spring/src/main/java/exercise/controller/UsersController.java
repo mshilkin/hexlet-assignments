7440cd841da30c222c4fdc623d660454c4d24e27
@hexlet/cli <support@hexlet.io>
2025-03-30 11:06:42 +0300

    submit java-advanced-ru/multithreading-spring

--- java-advanced-ru/multithreading-spring/src/main/java/exercise/controller/UsersController.java
+++ java-advanced-ru/multithreading-spring/src/main/java/exercise/controller/UsersController.java
@@ -2,19 +2,14 @@ package exercise.controller;
 
 import exercise.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.*;
 
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import exercise.service.UserService;
+
+import java.math.BigInteger;
 
 
 @RestController
@@ -30,6 +25,24 @@ public class UsersController {
     }
 
     // BEGIN
-    
+    @GetMapping("/{id}")
+    public Mono<User> getUser(@PathVariable("id") BigInteger id) {
+        return userService.findById(id);
+    }
+
+    @PostMapping
+    public  Mono<User> createUser(@RequestBody User user) {
+        return userService.createUser(user);
+    }
+
+    @PutMapping("/{id}")
+    public Mono<User> updateUser(@PathVariable BigInteger id, User user) {
+        return userService.updateUser(id, user);
+    }
+
+    @DeleteMapping("/{id}")
+    public Mono<Void> deleteUser(@PathVariable BigInteger id) {
+        return userService.deleteUser(id);
+    }
     // END
 }
--- java-advanced-ru/multithreading-spring/src/main/java/exercise/service/UserService.java
+++ java-advanced-ru/multithreading-spring/src/main/java/exercise/service/UserService.java
@@ -2,12 +2,16 @@ package exercise.service;
 
 import exercise.model.User;
 import exercise.repository.UserRepository;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Autowired;
 
 
+import org.springframework.web.server.ResponseStatusException;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import java.math.BigInteger;
 
 @Service
 public class UserService {
@@ -20,6 +24,26 @@ public class UserService {
     }
 
     // BEGIN
-    
+    public Mono<User> findById(BigInteger id) {
+        return userRepository.findById(id);
+    }
+    public Mono<User> createUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public Mono<User> updateUser(BigInteger id, User userDetails) {
+        return userRepository.findById(id)
+                .flatMap(user -> {
+                    user.setFirstName(userDetails.getFirstName());
+                    user.setLastName(userDetails.getLastName());
+                    //user.setEmail(userDetails.getEmail());
+                    return userRepository.save(user);
+                })
+                .switchIfEmpty(Mono.error(new ResponseStatusException(HttpStatus.NOT_FOUND,"User not found")));
+    }
+
+    public Mono<Void> deleteUser(BigInteger id) {
+        return userRepository.deleteById(id);
+    }
     // END
 }
