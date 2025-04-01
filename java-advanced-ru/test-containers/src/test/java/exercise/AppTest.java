402e17d0eef25677062211cbe8dd0dbaa7505d03
@hexlet/cli <support@hexlet.io>
2025-04-01 23:46:00 +0300

    submit java-advanced-ru/test-containers

--- java-advanced-ru/test-containers/src/test/java/exercise/AppTest.java
+++ java-advanced-ru/test-containers/src/test/java/exercise/AppTest.java
@@ -25,7 +25,10 @@ import org.testcontainers.containers.PostgreSQLContainer;
 @AutoConfigureMockMvc
 
 // BEGIN
-
+// Аннотация позволяет автоматически запускать и останавливать в тестах все контейнеры
+@Testcontainers
+// Все тесты выполняем в транзакции
+@Transactional
 // END
 public class AppTest {
 
@@ -33,11 +36,36 @@ public class AppTest {
     private MockMvc mockMvc;
 
     // BEGIN
-    
+    // Аннотация отмечает контейнер, который будет автоматически запущен
+    @Container
+    // Создаём контейнер с СУБД PostgreSQL
+    // В конструктор передаём имя образа, который будет скачан с Dockerhub
+    // Если не указать версию, будет скачана последняя версия образа
+    private static PostgreSQLContainer<?> database = new PostgreSQLContainer<>("postgres")
+            // Создаём базу данных с указанным именем
+            .withDatabaseName("dbname")
+            // Указываем имя пользователя и пароль
+            .withUsername("sa")
+            .withPassword("sa")
+            // Скрипт, который будет выполнен при запуске контейнера и наполнит базу тестовыми данными
+            .withInitScript("script.sql");
+
+    // Так как мы не можем знать заранее, какой URL будет у базы данных в контейнере
+    // Нам потребуется установить это свойство динамически
+    @DynamicPropertySource
+    public static void properties(DynamicPropertyRegistry registry) {
+        // Устанавливаем URL базы данных
+        registry.add("spring.datasource.url", database::getJdbcUrl);
+        // Имя пользователя и пароль для подключения
+        registry.add("spring.datasource.username", database::getUsername);
+        registry.add("spring.datasource.password", database::getPassword);
+        // Эти значения приложение будет использовать при подключении к базе данных
+    }
     // END
 
     @Test
     void testCreatePerson() throws Exception {
+        // Добавляем нового пользователя
         MockHttpServletResponse responsePost = mockMvc
             .perform(
                 post("/people")
@@ -49,6 +77,7 @@ public class AppTest {
 
         assertThat(responsePost.getStatus()).isEqualTo(200);
 
+        // И проверяем, что пользователь добавился в базу
         MockHttpServletResponse response = mockMvc
             .perform(get("/people"))
             .andReturn()
@@ -58,4 +87,6 @@ public class AppTest {
         assertThat(response.getContentType()).isEqualTo(MediaType.APPLICATION_JSON.toString());
         assertThat(response.getContentAsString()).contains("Jackson", "Bind");
     }
+    // Остальные тесты
+
 }
